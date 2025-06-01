24c9a897aba2fa6e05fbe3063cf562d1bd992d3f
P Sirisha <p.sirisha@ibm.com>
2025-06-01 20:33:16 +0530

    Initial commit

---
+++ LoginTest.java
@@ -0,0 +1,22 @@
+package bookcart;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://bookcart.azurewebsites.net/");
+		driver.findElement(By.name("Login")).click();
+		driver.findElement(By.name("Username"));
+		driver.quit();
+		
+	}
+	
+
+}
